c0414096c113b11b0a66c81cb33653af9fe0941a
stekajack <avantardejack@gmail.com>
2021-12-20 14:47:10 +0100

    sw_wip with catching and memoryviews

--- src/python/espressomd/stoner_wolfarth.pyx
+++ src/python/espressomd/stoner_wolfarth.pyx
@@ -8,61 +8,62 @@ from numpy cimport ndarray
 from .particle_data cimport ParticleHandle
 
 
-
 cdef class SW(object):
 
-    cdef double Hk  # Hk = 2*K1/Js
-    cdef double phi0
+    cdef float Hkinv  # Hk = 2*K1/Js
+    cdef double[:] phi0
+    cdef tuple[:] list_of_part_hndl
     cdef list nodes
-    cdef double p0
-    cdef double eps_phi
-    cdef double kT_KVm
-    cdef double p12
+    cdef float p0
+    cdef float eps_phi
+    cdef float kT_KVm_inv
+    cdef float p12
+    cdef float sat_magn
     cdef const double[:] e_k  
 
-    def __init__(self, double Hk, double kT_KVm, double eps_phi=1e-3):  # , axis, **kwargs):
-        self.Hk = Hk  # Hk = 2*K1/Js
-        #self.e_k = np.array(axis) / norm(axis)
-        self.phi0 = 0.
+    def __init__(self, float Hk, float kT_KVm, float sat_magn, tuple[:] list_of_part_hndl, float eps_phi=1e-3):  # , axis, **kwargs):
+        self.list_of_part_hndl = list_of_part_hndl
+        self.Hkinv = 1/Hk  # Hk = 2*K1/Js
+        self.phi0 = np.zeros(list_of_part_hndl.shape[0], dtype=float)
         self.nodes = []
         self.p0 = 1.
         self.eps_phi = eps_phi
-        self.kT_KVm = kT_KVm
+        self.kT_KVm_inv = 1/kT_KVm
         self.p12 = -1
+        self.sat_magn = sat_magn
     
     
-    cdef double phi(self, double theta, double h):
+    cdef float phi(self, float theta, float h, int index):
         
-        cdef double max1
-        cdef double max2
+        cdef float max1
+        cdef float max2
         fff = lambda phi: np.full(1, 0.25 - 0.25*cos(2*(phi-theta)) - h*cos(phi), dtype=float)
         dfff= lambda phi: np.full(1, 0.5*sin(2*(phi-theta))+h*sin(phi), dtype=float)
         invfff= lambda phi: np.full(1, -0.25 + 0.25*cos(2*(phi-theta)) + h*cos(phi), dtype=float)
         invdfff = lambda phi: np.full(1, -0.5*sin(2*(phi-theta))-h*sin(phi), dtype=float)
-        cdef double min1 = minimize(fff, x0=self.phi0+self.eps_phi, jac=dfff, method="BFGS", tol=1e-15, options={'gtol': 1e-15})['x'][0]
-        cdef double min2 = minimize(fff, x0=self.phi0+self.eps_phi - pi, jac=dfff, method="BFGS", tol=1e-15, options={'gtol': 1e-15})['x'][0]
+        cdef float min1 = minimize(fff, x0=self.phi0[index]+self.eps_phi, jac=dfff, method="BFGS", tol=1e-15, options={'gtol': 1e-15})['x'][0]
+        cdef float min2 = minimize(fff, x0=self.phi0[index]+self.eps_phi - np.pi, jac=dfff, method="BFGS", tol=1e-15, options={'gtol': 1e-15})['x'][0]
         
-        if min2 < -pi:
-            min2 += 2. * pi
-        elif min2 > pi:
-            min2 -= 2. * pi
+        if min2 < -np.pi:
+            min2 += 2. * np.pi
+        elif min2 > np.pi:
+            min2 -= 2. * np.pi
 
         if abs(min1 - min2) > 1.e-7:
-            max1 = minimize(invfff, x0=self.phi0+self.eps_phi, jac=invdfff, method="BFGS", tol=1e-15, options={'gtol': 1e-15})['x'][0]
+            max1 = minimize(invfff, x0=self.phi0[index]+self.eps_phi, jac=invdfff, method="BFGS", tol=1e-15, options={'gtol': 1e-15})['x'][0]
             max2 = minimize(invfff, x0=max1 - pi, jac=invdfff, method="BFGS", tol=1e-15, options={'gtol': 1e-15})['x'][0]
-            if max1 < -pi:
-                max1 += 2. * pi
-            elif max1 > pi:
-                max1 -= 2. * pi
-            if max2 < -pi:
-                max2 += 2. * pi
-            elif max2 > pi:
-                max2 -= 2. * pi
+            if max1 < -np.pi:
+                max1 += 2. * np.pi
+            elif max1 > np.pi:
+                max1 -= 2. * np.pi
+            if max2 < -np.pi:
+                max2 += 2. * np.pi
+            elif max2 > np.pi:
+                max2 -= 2. * np.pi
             b1 = fff(max1) - fff(min1)
             b2 = fff(max2) - fff(min1)
-            p12 = 0.5 * self.p0 * (exp(-b1 / self.kT_KVm) + exp(-b2 / self.kT_KVm))
-            self.p12 = p12
-            if random.random() < p12:
+            self.p12 = 0.5 * self.p0 * (exp(-b1 * self.kT_KVm_inv) + exp(-b2 * self.kT_KVm_inv))       
+            if random.random() < self.p12:
                 print("swap!")
                 self.nodes = [min2, min1, max2, max1]
                 sol = min2
@@ -73,34 +74,34 @@ cdef class SW(object):
             self.nodes = [min1]
             sol = min1
 
-        self.phi0 = sol
+        self.phi0[index] = sol
         return sol
 
-    cpdef void momtau(self, const double[:] H, list list_of_part_hndl):  # torque in reduced units (*mu0*ms*V for SI-units)
-        
-        cdef ParticleHandle part
+    cpdef void momtau(self, const double[:] H):  # torque in reduced units (*mu0*ms*V for SI-units)
+        cdef ParticleHandle part_real, part_virt
         cdef const double[:] axis
-        
-        for part in list_of_part_hndl:
-            axis = part.director
-            self.e_k = axis
-            H = np.array(H)
-            normH = np.linalg.norm(H)
-            e_h = H
-            h = 0.
+        cdef int index
+        cdef np.ndarray H_array = np.asarray(H)
+        cdef float normH = np.linalg.norm(H_array)
 
-            if normH > 0.:
-                e_h /= normH
-                h = normH / self.Hk
+        e_h = H_array/normH
+        h = normH * self.Hkinv
+        
+        for index in range(self.list_of_part_hndl.shape[0]):
+            part_real, part_virt = self.list_of_part_hndl[index]
+            axis = part_real.director
+            self.e_k = axis
+
             theta = acos(e_h.dot(self.e_k))
             axis = np.cross(e_h, self.e_k)
 
-            if theta > pi / 2.:
-                theta = pi - theta
+            if theta > np.pi / 2.:
+                theta = np.pi - theta
                 h = -h
                 e_h = -e_h
 
-            phi = self.phi(theta, h) % (2*pi)
-            e_p = np.cross(np.cross(e_h, self.e_k), e_h)
-            mom = e_h * cos(phi) + e_p * sin(phi)
-            part.dip=1.732*mom
+            phi = self.phi(theta, h, index) % (2*np.pi)
+            rotaxis = np.cross(e_h, self.e_k)
+            rotaxis /= np.linalg.norm(rotaxis)
+            mom = e_h * np.cos(phi) + np.cross(rotaxis, e_h) * np.sin(phi)
+            part_virt.dip=self.sat_magn*mom
